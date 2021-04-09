ff10d614e0af16302fdb30772773652e6d5f98f6
FrancescoMarcon <francesco.marcon@prorob.it>
2021-04-10 00:23:34 +0200

    add new template

--- Cerere/src/app/app-routing.module.ts
+++ Cerere/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: 'homepage', component:HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Cerere/src/app/app.module.ts
+++ Cerere/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
